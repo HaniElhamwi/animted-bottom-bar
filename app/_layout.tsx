079a6b69787221b74b031bf3c078ac56083884cb
HaniElhamwi <hanialhamwi000000@gmail.com>
2024-04-03 04:16:14 +0300

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,130 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+  useTheme,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useRef } from "react";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/components/useColorScheme";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
+import * as Animatable from "react-native-animatable";
+import { AntDesign } from "@expo/vector-icons";
+import ColorScreen from "@/components/ColorScreen";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+export { ErrorBoundary } from "expo-router";
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const TabArr = [
+  {
+    route: "home",
+    label: "Home",
+    activeIcon: "grid",
+    inActiveIcon: "grid-outline",
+    component: ColorScreen,
+  },
+  {
+    route: "Like",
+    label: "Like",
+    activeIcon: "heart-plus",
+    inActiveIcon: "heart-plus-outline",
+    component: ColorScreen,
+  },
+  {
+    route: "Search",
+    label: "Search",
+    activeIcon: "timeline-plus",
+    inActiveIcon: "timeline-plus-outline",
+    component: ColorScreen,
+  },
+  {
+    route: "Account",
+    label: "Account",
+    activeIcon: "user-circle",
+    inActiveIcon: "user-circle-o",
+    component: ColorScreen,
+  },
+];
+
+const animate1 = {
+  0: { scale: 0.5, translateY: 7 },
+  0.92: { translateY: -34 },
+  1: { scale: 1.2, translateY: -24 },
+};
+const animate2 = {
+  0: { scale: 1.2, translateY: -24 },
+  1: { scale: 1, translateY: 7 },
+};
+
+const circle1 = {
+  0: { scale: 0 },
+  0.3: { scale: 0.9 },
+  0.5: { scale: 0.2 },
+  0.8: { scale: 0.7 },
+  1: { scale: 1 },
+};
+const circle2 = { 0: { scale: 1 }, 1: { scale: 0 } };
+
+const TabButton = (props: any) => {
+  const { item, onPress, accessibilityState } = props;
+  const focused = accessibilityState.selected;
+  const viewRef: any = useRef(null);
+  const circleRef: any = useRef(null);
+  const textRef: any = useRef(null);
+  const isDarkMode = useColorScheme() === "dark";
+
+  const { colors } = useTheme();
+  const color = isDarkMode ? Colors.white : Colors.black;
+  const bgColor = colors.background;
+
+  useEffect(() => {
+    if (focused) {
+      viewRef.current.animate(animate1);
+      circleRef.current.animate(circle1);
+      textRef.current.transitionTo({ scale: 1 });
+    } else {
+      viewRef.current.animate(animate2);
+      circleRef.current.animate(circle2);
+      textRef.current.transitionTo({ scale: 0 });
+    }
+  }, [focused]);
+
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      activeOpacity={1}
+      style={styles.container}>
+      <Animatable.View ref={viewRef} duration={1000} style={styles.container}>
+        <View
+          style={[
+            styles.btn,
+            { borderColor: bgColor, backgroundColor: bgColor },
+          ]}>
+          <Animatable.View ref={circleRef} style={styles.circle} />
+          <AntDesign name="home" size={24} color="black" />
+        </View>
+        <Animatable.Text ref={textRef} style={[styles.text, { color }]}>
+          {item.label}
+        </Animatable.Text>
+      </Animatable.View>
+    </TouchableOpacity>
+  );
+};
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -44,15 +146,87 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
+const Tab = createBottomTabNavigator();
+
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <AnimTab1 />
     </ThemeProvider>
   );
 }
+
+function AnimTab1() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarStyle: {
+            height: 60,
+            position: "absolute",
+            margin: 16,
+            borderRadius: 16,
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        }}>
+        {TabArr.map((item, index) => {
+          return (
+            <Tab.Screen
+              key={index}
+              name={item.route}
+              component={item.component}
+              options={{
+                tabBarShowLabel: false,
+                tabBarButton: (props: any) => (
+                  <TabButton {...props} item={item} />
+                ),
+              }}
+            />
+          );
+        })}
+      </Tab.Navigator>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 70,
+  },
+  tabBar: {
+    height: 70,
+    position: "absolute",
+    margin: 16,
+    borderRadius: 16,
+  },
+  btn: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 4,
+    borderColor: Colors.white,
+    backgroundColor: Colors.white,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circle: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.primary,
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 12,
+    textAlign: "center",
+    color: Colors.primary,
+    fontWeight: "500",
+  },
+});
